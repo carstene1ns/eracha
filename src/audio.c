ab021db6cdacd70f8242aaca5edf9e8f0884a305
Carsten Teibes <dev@f4ke.de>
2021-10-18 14:51:07 +0200

    Fix Mixer init SDL2_mixer does not ship different .mod libraries anymore

--- src/audio.c
+++ src/audio.c
@@ -11,14 +11,10 @@
 static Mix_Music *music = NULL;
 
 int InitAudio() {
-  /* first try modplug */
-  int flags = MIX_INIT_MODPLUG;
-  if((Mix_Init(flags) & flags) != flags) {
-    /* second try mikmod */
-    flags = MIX_INIT_MOD;
-    if((Mix_Init(flags) & flags) != flags)
-      return 0;
-  }
+  /* enable mod(plug) */
+  int flags = MIX_INIT_MOD;
+  if((Mix_Init(flags) & flags) != flags)
+    return 0;
 
   if(Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 4096) == -1)
     return 0;

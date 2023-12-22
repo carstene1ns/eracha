2e771c6fb7db54632226ae25df7b37da5e4a30e8
Carsten Teibes <dev@f4ke.de>
2023-12-22 23:17:51 +0100

    Refactor and improve levelviewer

--- reverse_engineering/tools/src/levelviewer.c
+++ reverse_engineering/tools/src/levelviewer.c
@@ -1,12 +1,13 @@
 
 /******************************************************************************
  * levelviewer.c ERACHA level viewer
- * written by carstene1ns, 2014
+ * written by carstene1ns, 2014, 2023
  *****************************************************************************/
 
 #include <stdio.h>
 #include <ctype.h>
-
+#include <stdint.h>
+#include <stdbool.h>
 #include <SDL.h>
 #include <SDL_ttf.h>
 
@@ -16,8 +17,8 @@
 
 /* window parameters */
 #define WINDOW_TITLE "ERACHA level viewer"
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 768
 #define BARSIZE 24
 
 /* file parameters */
@@ -28,62 +29,88 @@
 /* typedefs make our life with structs easier */
 typedef struct
 {
-  int running, posx, posy, zoom, overlay, width, height, needsupdate;
-} tviewerstate;
+  bool running, needsupdate, checkerboard, dragging;
+  int zoom, overlay, width, height;
+  SDL_Point pos, mouseoffset, dragstart, dragoffset;
+} viewerstate_t;
 
 typedef struct
 {
   int width, height;
   char name[13];
-  unsigned char *background;
-  unsigned char *attributes;
-} tlevelinfo;
+  uint16_t **background;
+  uint32_t **attributes;
+} levelinfo_t;
 
 typedef struct
 {
   int size, count;
-} ttilesetinfo;
+} tilesetinfo_t;
 
 /* globals */
 SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
 SDL_Texture *tiletex = NULL;
 SDL_Texture *overlaytex = NULL;
+SDL_Texture *checkerboardtex = NULL;
+SDL_Cursor* hand_cursor = NULL;
+SDL_Cursor* cursor = NULL;
 TTF_Font *font = NULL;
 
 SDL_Color white = { 0xFF, 0xFF, 0xFF, 0 };
 
-tviewerstate viewerstate = { 1, 0, 0, 1, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 1 };
-tlevelinfo levelinfo = { 0, 0, "", NULL, NULL };
-ttilesetinfo tilesetinfo = { 0, 0 };
+viewerstate_t viewerstate = { true, true, true, false,
+  1, 0, WINDOW_WIDTH, WINDOW_HEIGHT,
+  {0, 0}, {0, 0}, {0, 0}, {0, 0} };
+levelinfo_t levelinfo = { 0, 0, "", NULL, NULL };
+tilesetinfo_t tilesetinfo = { 0, 0 };
 
 /* helper functions */
 void cleanup()
 {
+  if(font)
+    TTF_CloseFont(font);
+
   if (TTF_WasInit())
     TTF_Quit();
+
+  if(cursor)
+    SDL_FreeCursor(cursor);
+
+  if(tiletex)
+    SDL_DestroyTexture(tiletex);
+  if(checkerboardtex)
+    SDL_DestroyTexture(checkerboardtex);
+  if(overlaytex)
+    SDL_DestroyTexture(overlaytex);
+
+  if(renderer)
+    SDL_DestroyRenderer(renderer);
+
+  if(window)
+    SDL_DestroyWindow(window);
 
   SDL_Quit();
 }
 
-int init()
+bool init()
 {
   /* init SDL library */
   if(SDL_Init(SDL_INIT_VIDEO) < 0)
   {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
   /* always do cleanup */
   atexit(cleanup);
 
   window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_UNDEFINED,
-    SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
   if(window == NULL)
   {
     printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
   renderer = SDL_CreateRenderer(window, -1,
@@ -91,7 +118,7 @@ int init()
   if(renderer == NULL)
   {
     printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
   /* create some colors for our overlay */
@@ -100,7 +127,7 @@ int init()
   if(temp == NULL)
   {
     printf("Surface could not be created! SDL_Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
   for(int i = 0; i < 3; i++)
@@ -111,7 +138,7 @@ int init()
     d.w = 8;
     d.h = 8;
 
-    /* copy to big surface */
+    /* add different colors to surface */
     SDL_FillRect(temp, &d, SDL_MapRGBA(temp->format,
     (i == 0) ? 255 : 0, (i == 1) ? 255 : 0, (i == 2) ? 255 : 0, 100));
   }
@@ -120,14 +147,55 @@ int init()
   if(overlaytex == NULL)
   {
     printf("Texture could not be created! SDL_Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
   SDL_FreeSurface(temp);
+
+  /* create transparency effect */
+  #define BLOCKSIZE 8
+  temp = SDL_CreateRGBSurface(0, 2 * BLOCKSIZE, 2 * BLOCKSIZE, 32, 0x000000ff,
+    0x0000ff00, 0x00ff0000, 0xff000000);
+  if(temp == NULL)
+  {
+    printf("Surface could not be created! SDL_Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  for(int i = 0; i < 2; i++)
+  {
+    for(int j = 0; j < 2; j++)
+    {
+      SDL_Rect d = { i * BLOCKSIZE, j * BLOCKSIZE, BLOCKSIZE, BLOCKSIZE };
+
+      /* make checkerboard */
+      SDL_FillRect(temp, &d, i == j ? SDL_MapRGBA(temp->format, 128, 128, 128, 100) :
+        SDL_MapRGBA(temp->format, 255, 255, 255, 100));
+    }
+  }
+  #undef BLOCKSIZE
+
+  checkerboardtex = SDL_CreateTextureFromSurface(renderer, temp);
+  if(checkerboardtex == NULL)
+  {
+    printf("Texture could not be created! SDL_Error: %s\n", SDL_GetError());
+    return false;
+  }
+  SDL_FreeSurface(temp);
+
+  /* dragging cursor */
+  hand_cursor = SDL_CreateSystemCursor(SDL_SYSTEM_CURSOR_HAND);
+  if(hand_cursor == NULL)
+  {
+    printf("Cursor could not be created! SDL_Error: %s\n", SDL_GetError());
+    return false;
+  }
+  /* normal cursor */
+  cursor = SDL_GetCursor();
 
   /* init SDL2_TTF library */
   if(TTF_Init() == -1) {
     printf("Error while TTF_Init: %s\n", TTF_GetError());
-    return 0;
+    return false;
   }
 
   /* load font */
@@ -138,14 +206,14 @@ int init()
 #endif
   if(!font) {
     printf("Error while TTF_OpenFont: %s\n", TTF_GetError());
-    return 0;
+    return false;
   }
 
   /* all good */
-  return 1;
+  return true;
 }
 
-int loadlevel(char *levelfilename, char *attributesfilename)
+bool loadlevel(char *levelfilename, char *attributesfilename)
 {
   unsigned char *buf;
   unsigned long size;
@@ -153,20 +221,28 @@ int loadlevel(char *levelfilename, char *attributesfilename)
   /* load level file */
   buf = loadfile2(levelfilename, &size);
   if(buf == NULL)
-    return 0;
+    return false;
 
   /* parse file header */
   levelinfo.width = buf[ENDOFSMEHEADER - 8] | (buf[ENDOFSMEHEADER - 7] << 8);
   levelinfo.height = buf[ENDOFSMEHEADER - 4] | (buf[ENDOFSMEHEADER - 3] << 8);
 
   /* cache level in memory */
-  levelinfo.background = (unsigned char *)malloc(sizeof(unsigned char) * (size - ENDOFSMEHEADER));
+  levelinfo.background = (uint16_t **)malloc(sizeof(uint16_t *) * levelinfo.height + sizeof(uint16_t) * levelinfo.width * levelinfo.height);
   if(levelinfo.background == NULL)
   {
     perror("malloc");
-    return 0;
+    return false;
   }
-  memcpy(levelinfo.background, buf + ENDOFSMEHEADER, sizeof(unsigned char) * (size - ENDOFSMEHEADER));
+  uint16_t *bgptr = (uint16_t *)(levelinfo.background + levelinfo.height);
+  for(int y = 0; y < levelinfo.height; y++) {
+    levelinfo.background[y] = (bgptr + levelinfo.width * y);
+
+    for(int x = 0; x < levelinfo.width; x++) {
+      levelinfo.background[y][x] = (buf[ENDOFSMEHEADER + y * levelinfo.width * 2 + x * 2]) |
+        (buf[ENDOFSMEHEADER + y * levelinfo.width * 2 + x * 2 + 1] << 8);
+    }
+  }
 
   /* cleanup */
   free(buf);
@@ -176,28 +252,38 @@ int loadlevel(char *levelfilename, char *attributesfilename)
   /* load attributes file */
   buf = loadfile2(attributesfilename, &size);
   if(buf == NULL)
-    return 0;
+    return false;
 
   /* cache attributes in memory */
-  levelinfo.attributes = (unsigned char *)malloc(sizeof(unsigned char) * (size - ENDOFATRHEADER));
+  levelinfo.attributes = (uint32_t **)malloc(sizeof(uint32_t *) * levelinfo.height + sizeof(uint32_t) * levelinfo.width * levelinfo.height);
   if(levelinfo.attributes == NULL)
   {
     perror("malloc");
-    return 0;
+    return false;
   }
-  memcpy(levelinfo.attributes, buf + ENDOFATRHEADER, sizeof(unsigned char) * (size - ENDOFATRHEADER));
+  uint32_t *atrptr = (uint32_t *)(levelinfo.attributes + levelinfo.height);
+  for(int y = 0; y < levelinfo.height; y++) {
+    levelinfo.attributes[y] = (atrptr + levelinfo.width * y);
+
+    for(int x = 0; x < levelinfo.width; x++) {
+      levelinfo.attributes[y][x] = (buf[ENDOFATRHEADER + y * levelinfo.width * 4 + x * 4]) |
+        (buf[ENDOFATRHEADER + y * levelinfo.width * 4 + x * 4 + 1] << 8) |
+        (buf[ENDOFATRHEADER + y * levelinfo.width * 4 + x * 4 + 2] << 16) |
+        (buf[ENDOFATRHEADER + y * levelinfo.width * 4 + x * 4 + 3] << 24);
+    }
+  }
 
   /* cleanup */
   free(buf);
 
-  return 1;
+  return true;
 }
 
-int loadtileset(char *filename)
+bool loadtileset(char *filename)
 {
   unsigned char *buf = loadfile1(filename);
   if(buf == NULL)
-    return 0;
+    return false;
 
   /* parse file header */
   tilesetinfo.size = buf[ENDOFTILHEADER - 3];
@@ -208,7 +294,7 @@ int loadtileset(char *filename)
   if(pal == NULL)
   {
     perror("SDL_AllocPalette");
-    return 0;
+    return false;
   }
 
   SDL_Color col[255];
@@ -236,7 +322,7 @@ int loadtileset(char *filename)
   if(temp == NULL)
   {
     printf("Surface could not be created! SDL_Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
   /* copy tiles */
@@ -252,7 +338,7 @@ int loadtileset(char *filename)
       {
         printf("Surface could not be created from tile %d! SDL_Error: %s\n",
           (i * 40 + j), SDL_GetError());
-        return 0;
+        return false;
       }
 
       SDL_Rect d;
@@ -274,7 +360,7 @@ int loadtileset(char *filename)
   if(tiletex == NULL)
   {
     printf("Texture could not be created! SDL_Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
   /* cleanup */
@@ -282,7 +368,35 @@ int loadtileset(char *filename)
   SDL_FreePalette(pal);
   free(buf);
 
-  return 1;
+  return true;
+}
+
+void cliplevelpos() {
+  if(viewerstate.pos.y < -levelinfo.height)
+    viewerstate.pos.y = -levelinfo.height;
+
+  if(viewerstate.pos.y * 8 > viewerstate.height / viewerstate.zoom)
+    viewerstate.pos.y = (viewerstate.height / viewerstate.zoom) / 8;
+
+  if(viewerstate.pos.x < -levelinfo.width)
+    viewerstate.pos.x = -levelinfo.width;
+
+  if(viewerstate.pos.x * 8 > viewerstate.width / viewerstate.zoom)
+    viewerstate.pos.x = (viewerstate.width / viewerstate.zoom) / 8;
+}
+
+void scroll_x(bool isleft, bool with_shift)
+{
+  int amount = (with_shift ? 8 : 4);
+  viewerstate.pos.x += (isleft ? -amount : amount) / viewerstate.zoom;
+  viewerstate.needsupdate = 1;
+}
+
+void scroll_y(bool isup, bool with_shift)
+{
+  int amount = (with_shift ? 8 : 4);
+  viewerstate.pos.y += (isup ? -amount : amount) / viewerstate.zoom;
+  viewerstate.needsupdate = 1;
 }
 
 void zoom_in()
@@ -305,7 +419,7 @@ void zoom_out()
 
 void next_overlay()
 {
-  if(viewerstate.overlay < 31)
+  if(viewerstate.overlay < 30)
   {
     viewerstate.overlay++;
     viewerstate.needsupdate = 1;
@@ -321,6 +435,10 @@ void prev_overlay()
   }
 }
 
+int screen2tilepos(int v) {
+  return (v / viewerstate.zoom) / 8;
+}
+
 void checkevents()
 {
   SDL_Event e;
@@ -333,42 +451,25 @@ void checkevents()
     }
     else if(e.type == SDL_KEYDOWN)
     {
+      bool shift_pressed = e.key.keysym.mod & KMOD_SHIFT;
       switch(e.key.keysym.sym)
       {
         case SDLK_UP:
-          if(viewerstate.posy > -levelinfo.height)
-          {
-            viewerstate.posy -= 8 / viewerstate.zoom;
-            viewerstate.needsupdate = 1;
-          }
+          scroll_y(1, shift_pressed);
           break;
         case SDLK_DOWN:
-          if(viewerstate.posy * 8 < viewerstate.height / viewerstate.zoom)
-          {
-            viewerstate.posy += 8 / viewerstate.zoom;
-            viewerstate.needsupdate = 1;
-          }
+          scroll_y(0, shift_pressed);
           break;
         case SDLK_LEFT:
-          if(viewerstate.posx > -levelinfo.width)
-          {
-            viewerstate.posx -= 8 / viewerstate.zoom;
-            viewerstate.needsupdate = 1;
-          }
+          scroll_x(1, shift_pressed);
           break;
         case SDLK_RIGHT:
-          if(viewerstate.posx * 8 < viewerstate.width / viewerstate.zoom)
-          {
-            viewerstate.posx += 8 / viewerstate.zoom;
-            viewerstate.needsupdate = 1;
-          }
+          scroll_x(0, shift_pressed);
           break;
         case SDLK_PLUS:
-          /* zoom in */
           zoom_in();
           break;
         case SDLK_MINUS:
-          /* zoom out */
           zoom_out();
           break;
         case SDLK_PAGEUP:
@@ -377,9 +478,16 @@ void checkevents()
         case SDLK_PAGEDOWN:
           prev_overlay();
           break;
+        case SDLK_F5:
+          /* toggle transparency effect */
+          viewerstate.checkerboard ^= 1;
+          viewerstate.needsupdate = 1;
+          break;
         case SDLK_F6:
           /* reset window */
           SDL_SetWindowSize(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+          viewerstate.width = WINDOW_WIDTH;
+          viewerstate.height = WINDOW_HEIGHT;
           viewerstate.needsupdate = 1;
           break;
         case SDLK_q:
@@ -396,21 +504,40 @@ void checkevents()
     {
       if(e.wheel.y < 0)
       {
-        /* zoom out */
         zoom_out();
       }
       else if(e.wheel.y > 0)
       {
-        /* zoom in */
         zoom_in();
+      }
+    }
+    else if(e.type == SDL_MOUSEBUTTONDOWN)
+    {
+      if (!viewerstate.dragging && e.button.button == SDL_BUTTON_LEFT)
+      {
+        SDL_SetCursor(hand_cursor);
+        viewerstate.dragging = true;
+        viewerstate.dragoffset = viewerstate.mouseoffset;
+        viewerstate.dragstart = viewerstate.pos;
+      }
+    }
+    else if(e.type == SDL_MOUSEBUTTONUP)
+    {
+      if (viewerstate.dragging && e.button.button == SDL_BUTTON_LEFT)
+      {
+        SDL_SetCursor(cursor);
+        viewerstate.dragging = false;
       }
     }
     else if(e.type == SDL_MOUSEMOTION)
     {
-      if(e.motion.state & SDL_BUTTON_LMASK)
+      viewerstate.mouseoffset.x = screen2tilepos(e.motion.x);
+      viewerstate.mouseoffset.y = screen2tilepos(e.motion.y);
+
+      if (viewerstate.dragging)
       {
-        viewerstate.posx += (e.motion.xrel / viewerstate.zoom) / 8;
-        viewerstate.posy += (e.motion.yrel / viewerstate.zoom) / 8;
+        viewerstate.pos.x = viewerstate.dragstart.x - (viewerstate.dragoffset.x - viewerstate.mouseoffset.x);
+        viewerstate.pos.y = viewerstate.dragstart.y - (viewerstate.dragoffset.y - viewerstate.mouseoffset.y);
         viewerstate.needsupdate = 1;
       }
     }
@@ -436,12 +563,11 @@ void checkevents()
 void plotlevel()
 {
   /* nothing to do? */
-  if(viewerstate.needsupdate)
-    viewerstate.needsupdate = 0;
-  else
+  if(!viewerstate.needsupdate)
     return;
 
-  unsigned long block;
+  viewerstate.needsupdate = false;
+
   int xpos, ypos, line, column;
   SDL_Rect src, dest;
 
@@ -449,12 +575,27 @@ void plotlevel()
   SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
   SDL_RenderClear(renderer);
 
+  /* draw checkerboard */
+  if(viewerstate.checkerboard)
+  {
+    int w = 0, h = 0;
+    SDL_QueryTexture(checkerboardtex, NULL, NULL, &w, &h);
+    for(int x = 0; x < viewerstate.width; x+=w)
+    {
+      for(int y = BARSIZE; y < viewerstate.height; y+=w)
+      {
+        SDL_Rect d = { x, y, w, h };
+        SDL_RenderCopy(renderer, checkerboardtex, NULL, &d);
+      }
+    }
+  }
+
   for(int x = 0; x < levelinfo.width; x++)
   {
     for(int y = 0; y < levelinfo.height; y++)
     {
-      xpos = (x * 8 + viewerstate.posx * 8) * viewerstate.zoom;
-      ypos = BARSIZE + (y * 8 + viewerstate.posy * 8) * viewerstate.zoom;
+      xpos = (x * 8 + viewerstate.pos.x * 8) * viewerstate.zoom;
+      ypos = BARSIZE + (y * 8 + viewerstate.pos.y * 8) * viewerstate.zoom;
 
       /* make sure block is in window bounds */
       if(xpos < 0 || xpos > viewerstate.width ||
@@ -462,15 +603,14 @@ void plotlevel()
         continue;
 
       /* first: draw static objects */
-      block = (levelinfo.background[y * levelinfo.width * 2 + x * 2]) |
-              (levelinfo.background[y * levelinfo.width * 2 + x * 2 + 1] << 8);
+      uint16_t bgblock = levelinfo.background[y][x];
 
       /* skip empty blocks */
-      if(block != 0) {
+      if(bgblock != 0) {
 
         /* magic to get correct tile */
-        line = block / 40;
-        column = block - line * 40;
+        line = bgblock / 40;
+        column = bgblock - line * 40;
 
         src.x = column * tilesetinfo.size;
         src.y = line * tilesetinfo.size;
@@ -487,13 +627,10 @@ void plotlevel()
       }
 
       /* second: draw overlay */
-      block = (levelinfo.attributes[y * levelinfo.width * 4 + x * 4]) |
-              (levelinfo.attributes[y * levelinfo.width * 4 + x * 4 + 1] << 8) |
-              (levelinfo.attributes[y * levelinfo.width * 4 + x * 4 + 2] << 16) |
-              (levelinfo.attributes[y * levelinfo.width * 4 + x * 4 + 3] << 24);
+      uint32_t olblock = levelinfo.attributes[y][x];
 
-      /* -1 is special */
-      if(viewerstate.overlay == -1 && block == 0)
+      /* -1 is special, denotes all blocks without special handling */
+      if(viewerstate.overlay == -1 && olblock == 0)
       {
         SDL_Rect src, dest;
 
@@ -512,15 +649,15 @@ void plotlevel()
       }
       else
       {
-        double flag = pow(2, viewerstate.overlay);
+        float flag = powf(2, viewerstate.overlay);
 
         /* skip other blocks */
-        if(block & (int)flag)
+        if(olblock & (int)flag)
         {
           SDL_Rect src, dest;
 
           src.x = 0;
-          src.y = 0;
+          src.y = (viewerstate.overlay > 8 ? 16: 0);
           src.w = tilesetinfo.size;
           src.h = tilesetinfo.size;
 
@@ -538,11 +675,12 @@ void plotlevel()
 
   /* update title bar */
   char title[100];
-  sprintf(title, "level: %s, %dx%d blocks | window: %dx%d | pos: %dx%d | zoom: %dx | overlay: %d",
+  sprintf(title, "level: %s, %dx%d blocks | window: %dx%d | pos: %dx%d | zoom: %dx | overlay: %d(%d)",
     levelinfo.name, levelinfo.width, levelinfo.height,
     viewerstate.width, viewerstate.height,
-    viewerstate.posx, viewerstate.posy,
-    viewerstate.zoom, viewerstate.overlay);
+    viewerstate.pos.x, viewerstate.pos.y,
+    viewerstate.zoom, viewerstate.overlay,
+    (int)pow(2, viewerstate.overlay));
 
   SDL_Surface *text = TTF_RenderText_Solid(font, title, white);
   SDL_Texture *texttex = SDL_CreateTextureFromSurface(renderer, text);
@@ -588,14 +726,14 @@ int main(int argc, char *argv[])
   if(attributesfile == NULL)
   {
     perror("malloc");
-    return 0;
+    exit(1);
   }
 
   tilesetfile = (char *)malloc(sizeof(char) * strlen(levelfile) + 1);
   if(tilesetfile == NULL)
   {
     perror("malloc");
-    return 0;
+    exit(1);
   }
   strcpy(attributesfile, levelfile);
   strcpy(tilesetfile, levelfile);
@@ -608,21 +746,24 @@ int main(int argc, char *argv[])
 
   /* setup window */
   if(!init())
-    exit(0);
+    exit(1);
 
   /* load data */
   if(!loadlevel(levelfile, attributesfile))
-    exit(0);
+    exit(1);
 
   /* load gfx */
   if(!loadtileset(tilesetfile))
-    exit(0);
+    exit(1);
 
   /* main loop */
   while(viewerstate.running)
   {
     /* check for key presses and other events */
     checkevents();
+
+    /* clip position after scrolling */
+    cliplevelpos();
 
     /* update display */
     plotlevel();
@@ -634,9 +775,6 @@ int main(int argc, char *argv[])
   /* deinit */
   free(attributesfile);
   free(tilesetfile);
-  TTF_CloseFont(font);
-  SDL_DestroyTexture(tiletex);
-  SDL_DestroyWindow(window);
 
   exit(0);
 }
